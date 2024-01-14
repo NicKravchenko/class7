9fa3545b0e80d0ecadb11c99487e248884d4313d
nickravchenko <nickravchenko98@gmail.com>
2024-01-14 19:02:23 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:class7/screens/home_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ThemeData().colorScheme.copyWith(
+              primary: Colors.grey,
+              secondary: Colors.black,
+            ),
+      ),
+      home: const HomeScreen(
+        title: "Recipe App",
+      ),
+    );
+  }
+}
